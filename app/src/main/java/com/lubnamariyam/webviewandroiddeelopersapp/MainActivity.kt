6146ea9231dd82e8d5ed268e1451b180b7e1d25c
Lubna Mariyam <lubnamariyam@ajro.com>
2021-11-16 01:21:43 +0530

    Initial commit

---
+++ app/src/main/java/com/lubnamariyam/webviewandroiddeelopersapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.lubnamariyam.webviewandroiddeelopersapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lubnamariyam.webviewandroiddeelopersapp.ui.theme.WebviewAndroidDeelopersAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebviewAndroidDeelopersAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    webPage("https://in.oriflame.com/")
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WebviewAndroidDeelopersAppTheme {
+        webPage("www.google.com")
+
+    }
+}
