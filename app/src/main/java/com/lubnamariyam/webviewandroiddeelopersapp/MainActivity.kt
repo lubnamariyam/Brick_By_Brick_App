39c84a8de6345177a062b1943b5f9b2d6ecccb76
Lubna Mariyam <lubnamariyam@ajro.com>
2021-11-23 20:53:36 +0530

    initial commit

--- app/src/main/java/com/lubnamariyam/webviewandroiddeelopersapp/MainActivity.kt
+++ app/src/main/java/com/lubnamariyam/webviewandroiddeelopersapp/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.lubnamariyam.webviewandroiddeelopersapp
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.lubnamariyam.webviewandroiddeelopersapp.ui.theme.WebviewAndroidDeelopersAppTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +38,32 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WebviewAndroidDeelopersAppTheme {
-        webPage("www.google.com")
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
 
+    // Animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            // tween Animation
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }))
+        // Customize the delay time
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        // Change the logo
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value))
     }
 }
